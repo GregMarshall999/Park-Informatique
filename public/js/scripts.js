f645c5255dc1f8bdc457060d154f0c2e7df67ee3
GregMarshall999 <gregory.marshall999@gmail.com>
2024-06-18 12:14:21 +0200

    added home and login page

---
+++ public/js/scripts.js
@@ -0,0 +1,28 @@
+document.getElementById('login-form').addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('error-message');
+
+    try {
+        const response = await fetch('http://localhost:3000/api/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password }),
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            throw new Error(data.message);
+        }
+
+        localStorage.setItem('token', data.token);
+        window.location.href = 'index.html';
+    } catch (error) {
+        errorMessage.textContent = error.message;
+    }
+});
--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const createCrudRoutes = require('./routes/crudHandler');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const path = require('path');
 
 const app = express();
 
-//ajout de bodyparser
+//ajout de middleware
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 //connection a DB
 mongoose.connect('mongodb://localhost:27017/mydatabase', {
@@ -18,6 +22,23 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
   console.log('Connected to MongoDB');
+});
+
+//Route de connexion
+app.post('/api/auth/login', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username });
+    if (!user) return res.status(400).json({ message: 'Nom d`Utilisateur ou mot de passe Invalide' });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: 'Nom d`Utilisateur ou mot de passe Invalide' });
+
+    const token = jwt.sign({ userId: user._id }, 'jwt_secret'); //a tester
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 // Ajout des Models
